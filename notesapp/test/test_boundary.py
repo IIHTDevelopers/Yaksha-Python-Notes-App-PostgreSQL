4b227a971a6a6db92185ccab71450153cf52bdd4
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-02-13 13:54:47 +0530

    Delete notesapp directory

--- notesapp/test/test_boundary.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.test import APITestCase
-from notesapp.test.TestUtils import TestUtils
-class NotesAppAPIBoundaryTest(APITestCase):
-    def test_boundary(self):
-        test_obj = TestUtils()
-        test_obj.yakshaAssert("TestBoundary",True,"boundary")
-        print("TestBoundary = Passed")
--- notesapp/test/test_exceptional.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.test import APITestCase
-from notesapp.models import NotesModel
-from notesapp.test.TestUtils import TestUtils
-class NotesAppAPIExceptionalTest(APITestCase):
-    def test_exception(self):
-        test_obj = TestUtils()
-        test_obj.yakshaAssert("TestException",True,"exception")
-        print("TestException = Passed")
